7f66bd4f467e7c7f8e3f364bc41aaedf472f4b75
bukwon <dnu05043@naver.com>
2023-12-07 11:26:01 +0900

    ë¡로그인 및 로그아웃, 회원가입 설정  완료

---
+++ src/main/java/com/example/mediumproject/SecurityConfig.java
@@ -0,0 +1,46 @@
+package com.example.mediumproject;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.header.writers.frameoptions.XFrameOptionsHeaderWriter;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    @Bean
+    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests((authorizeHttpRequests) ->
+                authorizeHttpRequests.requestMatchers(new AntPathRequestMatcher("/**")).
+                permitAll())
+                .csrf((csrf) -> csrf
+                        .ignoringRequestMatchers(new AntPathRequestMatcher("/h2-console/**")))
+                .headers((headers) -> headers.addHeaderWriter(new XFrameOptionsHeaderWriter(
+                        XFrameOptionsHeaderWriter.XFrameOptionsMode.SAMEORIGIN
+                )))
+                .formLogin((formLogin) -> formLogin.loginPage("/home/login")
+                        .loginProcessingUrl("/home/login")  // 405 --> home 디렉토리 추가 안할 시 기본 /login으로 파싱 ..()
+                        .defaultSuccessUrl("/"))    // 로그인 성공시 home루트로 돌아가지만 나중에 다듬도록 하자
+                .logout((logout) -> logout.logoutRequestMatcher(new AntPathRequestMatcher("/home/logout"))
+                        .logoutSuccessUrl("/")
+                        .invalidateHttpSession(true));
+        return http.build();
+    }
+
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception{
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+}
---
+++ src/test/java/com/example/mediumproject/MediumProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mediumproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MediumProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
