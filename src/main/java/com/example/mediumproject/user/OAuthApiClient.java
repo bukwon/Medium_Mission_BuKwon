bc51b81b06f008986393c0be59d8bd976880034d
bukwon <dnu05043@naver.com>
2023-12-14 13:44:39 +0900

    카카오 로그인 우선 삭제

--- src/main/java/com/example/mediumproject/user/OAuthApiClient.java
+++
@@ -1,7 +0,0 @@
-package com.example.mediumproject.user;
-
-public interface OAuthApiClient {
-    OAuthProvider oAuthProvider();
-    String requestAccessToken(OAuthLoginParams params);
-    OAuthInfoResponse requestOauthInfo(String accessToken);
-}
--- src/main/java/com/example/mediumproject/user/OAuthInfoResponse.java
+++
@@ -1,7 +0,0 @@
-package com.example.mediumproject.user;
-
-public interface OAuthInfoResponse {
-    String getEmail();
-    String getNickname();
-    OAuthProvider getOAuthProvider();
-}
